621c236a4bd10f80450b77cec4178fcff0b6e204
M. Tissot Evan <tissot@insa-08125>
2019-01-10 10:41:12 +0100

    Database

--- src/Database.java
+++ src/Database.java
@@ -3,46 +3,29 @@ import java.sql.*;
 
 public class Database {
 
-	   // JDBC driver name and database URL
-	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	   static final String DB_URL = "jdbc:mysql://localhost/mysql?serverTimezone=UTC";
-
-	   //  Database credentials
-	   static final String USER = "Clavardage";
-	   static final String PASS = "guerrier";
-	   
-	   public static void main(String args[]) {
-	   User user= new User("loic");
+		private static User mainUser;
+	
+		
+	
+	   public static void createDB(Controler c) {
+		   
+		   
+		   mainUser = c.mainUser;
+		   String DB_URL = "jdbc:sqlite:Clavardage - "+ mainUser.getPseudo()+".db";
 
 	   Connection conn = null;
 	   Statement stmt = null;
 	   try{
-	      //STEP 2: Register JDBC driver
-	      //Class.forName("sun.jdbc.odbcJdbcOdbcDriver");
-		     
-		  Class.forName("com.mysql.cj.jdbc.Driver");
 
-	      //STEP 3: Open a connection
 	      System.out.println("Connecting to database...");
-	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	      conn = DriverManager.getConnection(DB_URL);
 
 	      //STEP 4: Execute a query
 	      System.out.println("Creating database...");
 	      stmt = conn.createStatement();
 	      
-	      String sql = "CREATE DATABASE IF NOT EXISTS CLAVARDAGE ";
-	      stmt.executeUpdate(sql);
-	      System.out.println("Database created successfully...");
 	      
-	      sql = "use CLAVARDAGE";
-	      stmt.executeUpdate(sql);
-	      
-	      //Cree la table correspondant au pseudo
-	      sql = "CREATE TABLE IF NOT EXISTS "+ user.getPseudo()+"(PSEUDO VARCHAR(20),MESSAGE VARCHAR(150))";
-	      stmt.executeUpdate(sql);
 
-	      sql = "INSERT INTO "+ user.getPseudo()+" VALUES ('EVAN','BONJOUR ! ')";
-	      stmt.executeUpdate(sql);
 
 	   }catch(SQLException se){
 	      //Handle errors for JDBC
@@ -67,6 +50,59 @@ public class Database {
 	   System.out.println("Goodbye!");
 	}//end createDB
 
+	   public static void write(Message m, boolean send)
+	   {
+		   Connection conn = null;
+		   Statement stmt = null;
+		   try{
+
+			   String DB_URL = "jdbc:sqlite:Clavardage - "+ mainUser.getPseudo()+".db";
+			   String tableName;
+
+		      //STEP 3: Open a connection
+		      conn = DriverManager.getConnection(DB_URL);
+
+		      //STEP 4: Execute a query
+		      stmt = conn.createStatement();
+		      
+		      //Cree la table correspondant au pseudo
+		      if(send)   // MainUser est la source
+		    	tableName =  m.getUserDest().getPseudo();
+		      else // MainUser recoit le message
+			    tableName =  m.getUserSrc().getPseudo();		      
+		    	  
+		      String sql = "CREATE TABLE IF NOT EXISTS "+ tableName +"(PSEUDO VARCHAR(20),MESSAGE VARCHAR(150))";
+		      stmt.executeUpdate(sql);
+		      
+		      
+		      sql = "INSERT INTO " + tableName + " VALUES ('" + m.getUserSrc() + "' , '" + m.getPDU() + "')"; 
+		      stmt.executeUpdate(sql);
+		      
+		      
+		      
+		   		}
+		   catch(SQLException se){
+			      //Handle errors for JDBC
+			      se.printStackTrace();
+			   }catch(Exception e){
+			      //Handle errors for Class.forName
+			      e.printStackTrace();
+			   }finally{
+			      //finally block used to close resources
+			      try{
+			         if(stmt!=null)
+			            stmt.close();
+			      }catch(SQLException se2){
+			      }// nothing we can do
+			      try{
+			         if(conn!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }//end finally try
+			   }//end try
+	   
+	   }  
 	   
 	   
 }
--- src/Message.java
+++ src/Message.java
@@ -86,6 +86,15 @@ public class Message implements Serializable {
 	  return this.userOri;
   }
   
+  public User getUserDest()
+  {
+	  return this.userDest;
+  }  
+  
+  public String getPDU()
+  {
+	  return this.pdu;
+  }
   
   public String toString()
   {
